a323bc6b3b04190c7b2114a982ac197df846aa1c
khalid almizel <khalid.almizel98@gmail.com>
2018-04-23 22:30:39 +0300

    added the java class
    i added the jave class

---
+++ src/TestMultipleCatchBlock.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class TestMultipleCatchBlock{  
+  public static void main(String args[]){  
+  try {
+	  int []arr=new int [10];
+	  int a,b;
+	  Scanner input = new Scanner(System.in);
+	  a=input.nextInt();
+	  b=input.nextInt();
+	  int res= a/b;
+	  arr[10]=a/b;
+
+	  System.out.println(res);
+  }catch(ArithmeticException e) {
+System.out.println(e.toString());	  
+  }
+  catch(RuntimeException e) {
+System.out.println(e.toString());
+  }
+ 
+ }  
+}  
+
